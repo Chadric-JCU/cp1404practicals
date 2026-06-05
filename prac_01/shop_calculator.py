6806fb0cc3949374d2c060c3156db16d818ae051
Chadric <junkaichadric.kok@my.jcu.edu.au>
2026-06-05 19:02:05 +0800

    Add shop_calculator.py

---
+++ prac_01/shop_calculator.py
@@ -0,0 +1,19 @@
+from zmq import DISCONNECT_MSG
+
+DISCOUNT_RATE = 0.9
+DISCOUNT_THRESHOLD = 100
+total_price = 0
+
+number_of_items = int(input("Number of items: "))
+while number_of_items <= 0:
+    print("Invalid number of items!")
+    number_of_items = int(input("Number of items: "))
+
+for i in range(number_of_items):
+    item_price = float(input("Price of item: "))
+    total_price += item_price
+
+if total_price > DISCOUNT_THRESHOLD:
+    total_price *= DISCOUNT_RATE
+
+print(f"Total price for {number_of_items} item(s) is ${total_price:.2f}")
