bf2c33b2681ed7eed55f396da6a49f882be6b2cc
Chadric <junkaichadric.kok@my.jcu.edu.au>
2026-06-08 16:58:04 +0800

    Rewrite score checker into functions

--- prac_02/scores.py
+++ prac_02/scores.py
@@ -3,12 +3,28 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+def main():
+    score = float(input("Enter score: "))
+    grade = determine_grade(score)
+    print(f"User score {score} is {grade}")
+    if grade == "Excellent":
+        print("YOu get a prize!")
+    random_score = random.randint(0, 100)
+    grade = determine_grade(random_score)
+    print(f"Random: {random_score} = {grade} ")
+
+def determine_grade(score):
+    if score < 0 or score > 100:
+        grade = "Invalid score"
+    elif score >= 90:
+        grade = "Excellent"
+    elif score >= 50:
+        grade= "Passable"
+    else:
+        grade = "Bad"
+
+    return grade
+
+main()
