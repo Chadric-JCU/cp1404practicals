661121a44e8cc9f8c7a2f979ffbeb2d354696f69
Chadric <junkaichadric.kok@my.jcu.edu.au>
2026-06-08 14:29:07 +0800

    Refactor password check program to use functions

---
+++ prac_02/password_stars.py
@@ -0,0 +1,22 @@
+'''Takes a password and replaces it with stars'''
+
+MINIMUM_LENGTH = 5
+
+def main():
+    password = get_password()
+    print_stars(password)
+
+def get_password() -> str:
+    password = input("Enter password to censor: ")
+    while len(password) < MINIMUM_LENGTH:
+        print(f"Password too short, min length is {MINIMUM_LENGTH}")
+        password = input("Enter a password: ")
+    return password
+
+
+def print_stars(password):
+    for letter in password:
+        print("*", end="")
+
+main()
+
